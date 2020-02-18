7580ae8ca09e76ec8e85f8eb3bb0f1e2e962a5f6
rangamneeraja <50659221+rangamneeraja@users.noreply.github.com>
2020-02-18 19:43:03 +0530

    core data

---
+++ CoreDataCRUD/CoreDataCRUD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataCRUD
+//
+//  Created by neeraja on 12/02/20.
+//  Copyright © 2020 neeraja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
