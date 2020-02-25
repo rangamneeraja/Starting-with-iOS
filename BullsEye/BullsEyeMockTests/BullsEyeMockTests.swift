dbf1ec37ec3be579b252dc45fd4b5f784f1a31c2
rangamneeraja <50659221+rangamneeraja@users.noreply.github.com>
2020-02-25 15:02:36 +0530

    bulls eye type 2 test project

--- BullsEye/BullsEyeMockTests/BullsEyeMockTests.swift
+++ BullsEye/BullsEyeMockTests/BullsEyeMockTests.swift
@@ -26,9 +26,48 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
+import XCTest
+@testable import BullsEye
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+class MockUserDefaults: UserDefaults {
+  var gameStyleChanged = 0
+  override func set(_ value: Int, forKey defaultName: String) {
+    if defaultName == "gameStyle" {
+      gameStyleChanged += 1
+    }
+  }
+}
+
+class BullsEyeMockTests: XCTestCase {
+  var sut: ViewController!
+  var mockUserDefaults: MockUserDefaults!
+  
+  override func setUp() {
+    super.setUp()
+    
+    sut = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController() as? ViewController
+    mockUserDefaults = MockUserDefaults(suiteName: "testing")
+    sut.defaults = mockUserDefaults
+  }
+  
+  override func tearDown() {
+    sut = nil
+    mockUserDefaults = nil
+    super.tearDown()
+  }
+
+  func testGameStyleCanBeChanged() {
+    // given
+    let segmentedControl = UISegmentedControl()
+    
+    // when
+    XCTAssertEqual(mockUserDefaults.gameStyleChanged, 0, "gameStyleChanged should be 0 before sendActions")
+    segmentedControl.addTarget(sut,
+                               action: #selector(ViewController.chooseGameStyle(_:)), for: .valueChanged)
+    segmentedControl.sendActions(for: .valueChanged)
+    
+    // then
+    XCTAssertEqual(mockUserDefaults.gameStyleChanged, 1, "gameStyle user default wasn't changed")
+  }
+
 }
