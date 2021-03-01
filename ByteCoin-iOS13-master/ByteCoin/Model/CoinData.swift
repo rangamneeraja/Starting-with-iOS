c1b83fc113c1c6accc0367733fa83cb0b7e95ea9
rangamneeraja <50659221+rangamneeraja@users.noreply.github.com>
2021-03-01 16:44:32 +0530

    bytecoin

---
+++ ByteCoin-iOS13-master/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Rexel_Neeraja on 13/10/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable{
+    let rate: Double
+}
